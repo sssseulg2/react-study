3e71bbc14d9d4d159ab64d59eac24b9b556ac002
이슬기 <iseulgizzz@gmail.com>
2022-07-23 17:27:10 +0900

    made tab / detail

--- shopping-mall/src/pages/Detail.js
+++ shopping-mall/src/pages/Detail.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react';
+import { Nav } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import data from '../data';
 
@@ -9,6 +10,8 @@ function Detail() {
     let shoes = useState(data)[0];
     let {id} = useParams();
     let number = Number(id)+1;
+
+    let [tab, setTab] = useState(0)
 
     useEffect(() => {
         setTimeout(() => { setSale(false); }, 2000)
@@ -25,7 +28,6 @@ function Detail() {
             }
             
             <div className="row">
-                
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes"+ number +".jpg"} width="100%" alt=""/>
                 </div>
@@ -38,8 +40,27 @@ function Detail() {
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
+
+            <Nav variant="tabs"  defaultActiveKey="btn0">
+                <Nav.Item>
+                    <Nav.Link eventKey="btn0" onClick={()=>{setTab(0)}}>버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="btn1" onClick={()=>{setTab(1)}}>버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="btn2" onClick={()=>{setTab(2)}}>버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab}/>
         </div> 
     )
+}
+
+function TabContent({tab}) {
+    return [<div>내용0</div>,
+            <div>내용1</div>,
+            <div>내용2</div> ][tab]
 }
 
 export default Detail;
